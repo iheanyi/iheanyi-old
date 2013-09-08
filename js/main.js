6421be87a9f4cd5bd3216ae0af57f3af0642310b
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-09-07 22:22:55 -0400

    Add JavaScript files.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	//$.scrollIt();
+
+	$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'linear',      // the easing function for animation
+  scrollTime: 600,       // how long (in ms) the animation takes
+  activeClass: 'pure-menu-selected', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: 0          // offste (in px) for fixed top navigation
+});
+	console.log("READY!");
+});
