9dc213d9480f52905f93c3add824c72db05002f3
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-11-18 22:22:46 -0500

    New additions, new resume.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
 	//$.scrollIt();
 
+/*  jQuery.getFeed({
+     url     : 'http://blog.iheanyi.com/rss/',
+     success : function (feed) {
+        console.log(feed.title);
+        // do more stuff here
+     }
+  }); */
+
 	$.scrollIt({
   upKey: 38,             // key code to navigate to the next section
   downKey: 40,           // key code to navigate to the previous section
@@ -9,6 +17,16 @@ $( document ).ready(function() {
   activeClass: 'pure-menu-selected', // class given to the active nav element
   onPageChange: null,    // function(pageIndex) that is called when page is changed
   topOffset: -40         // offste (in px) for fixed top navigation
-});
+  });
+
+
+    $('.posts').rssfeed('http://blog.iheanyi.com/rss/', {
+        limit: 5,
+        header: false,
+        titletag: 'h2',
+        dateformat: 'date'
+    });
+
 	console.log("READY!");
+
 });
